c74254d0e4d972c8b6735cd068d5f349e92683c5
Wu Minzhe <wuminzhe@gmail.com>
2019-07-17 14:45:27 +0800

    1. fix bid order volume after trading; 2. use bigdecimal in fix order price and volume precision before add to database

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use mysql::*;
 use amiquip::{FieldTable, ExchangeType, ExchangeDeclareOptions, Connection, ConsumerMessage, ConsumerOptions, QueueDeclareOptions, Result};
 
-// extern crate intrusive_collections;
-
 mod engine;
 mod models;
 mod managers;
@@ -17,13 +15,14 @@ fn main(){
 
     let on_trade = |event: TradeEvent| {
         println!("price: {}, volume: {}", event.price, event.volume);
+        // TODO: 隔离级别需要调整
         &pool
             .start_transaction(false, Some(IsolationLevel::RepeatableRead), Some(false))
             .and_then(|mut tx| {
                 Trade::create(&mut tx, event.price, event.volume, event.ask_order_id, event.bid_order_id);
 
                 Order::sub_volume(&mut tx, event.ask_order_id, event.volume, event.ask_order_filled);
-                Order::add_volume(&mut tx, event.bid_order_id, event.volume, event.bid_order_filled);
+                Order::sub_volume(&mut tx, event.bid_order_id, event.volume, event.bid_order_filled);
                 tx.commit()
             });
     };
@@ -68,6 +67,7 @@ fn main(){
 
                     let id = order_manager.submit(price, volume, side, user_id).unwrap();
                     // println!("({:>3}) Received [{}]", i, body);
+                    // TODO: 考虑去掉ack
                     consumer.ack(delivery);
                 }
             }
@@ -79,34 +79,4 @@ fn main(){
     }
 
     connection.close();
-
-    // let path = "./src/orders.csv";
-    // let input = File::open(path);
-    // let buffered = BufReader::new(input);
-    // for line in buffered.lines() {
-        // let row = line;
-        // let mut split = row.split(",");
-        // let id: u32 = split.next().unwrap().parse().unwrap();
-        // let price: f64 = split.next().unwrap().parse().unwrap();
-        // let volume: f64 = split.next().unwrap().parse().unwrap();
-        // let order_type = split.next().unwrap();
-        // let side = if order_type == "OrderAsk" {
-            // Side::Sell
-        // } else {
-            // Side::Buy
-        // };
-        // let limit_order = LimitOrder::new(
-            // id,
-            // side,
-            // volume,
-            // price,
-        // );
-        // engine.submit(limit_order);
-    // }
-    
-
-
-    // order_manager.submit(0.12, 11.000000035, 0, "u123456");
-    // order_manager.submit(0.13, 11.00000003, 1, "u123456");
-
 }
